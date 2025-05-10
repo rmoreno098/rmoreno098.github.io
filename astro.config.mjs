1d9ea64878cabb3346733242cfc653b0ed7fb2c4
roman <roman.moreno226@live.com>
2025-05-09 22:35:48 -0500

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  site: "https://rmoreno098.github.io",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
